5403d8669c4ee73d13943adaf1e3f4e7698f05ed
Rafael Eduardo Melo Rodriguez <rafaeleduardomelorodriguez@gmail.com>
2023-12-11 01:38:20 -0500

    fix: ProfessorRepository

--- grading_api/src/main/java/com/vanier/grading_api/repository/ProfessorRepository.java
+++ grading_api/src/main/java/com/vanier/grading_api/repository/ProfessorRepository.java
@@ -1,9 +1,11 @@
 package com.vanier.grading_api.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.vanier.grading_api.entity.Professor;
 
+@Repository
 public interface ProfessorRepository extends CrudRepository<Professor, Long> {
 
 }
