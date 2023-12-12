02163f58381ee97bc7612fcee4fd66ef9256f847
Kelly Vergara T <kellyvergara12@gmail.com>
2023-12-11 22:20:07 -0500

    feat: testing

---
+++ grading_api/src/test/java/com/vanier/grading_api/controller/AssignatureControllerTest.java
@@ -0,0 +1,137 @@
+package com.vanier.grading_api.controller;
+
+//static imports required for the payload
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.vanier.grading_api.entity.Assignature;
+import com.vanier.grading_api.service.AssignatureService;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AssignatureControllerTest {
+
+    //this injects the service with the implementation class
+    @MockBean AssignatureService service;
+
+    @Autowired MockMvc mockMvc;
+
+    @Test
+    void testDelete() throws JsonProcessingException, Exception {
+        //Setup
+        Assignature assignature = new Assignature();
+        assignature.setId(1L);
+        assignature.setName("software dev");
+        assignature.setCode("abc123");      
+        assignature.setCredits("4");
+        assignature.setStatus("active");
+        
+        // Mock the service findById and delete method since they work within
+        //the delete controller method
+        Mockito.when(service.findById(Mockito.anyLong()))
+        .thenReturn(Optional.of(assignature));
+        Mockito.doNothing().when(service).delete(Mockito.anyLong());
+
+        //Execute
+        mockMvc.perform(
+            delete("/assignature/delete?id=1")).andDo(print())
+        //Assertions
+            .andExpect(status().isAccepted())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("software dev")); 
+    }
+
+    @Test
+    void testFindById() throws Exception {
+        //set up
+        Assignature assignature = new Assignature();
+        assignature.setId(1L);
+        assignature.setCode("abc123");      
+        assignature.setName("software dev");
+        assignature.setStatus("active");
+
+        Mockito.when(service.findById(Mockito.anyLong()))
+        .thenReturn(Optional.of(assignature));
+
+        //Execute
+        mockMvc.perform(
+            get("/assignature/findById?id=1")).andDo(print())
+        //Assert
+        .andExpect(status().isFound());
+    }
+
+    @Test
+    void testSave() throws Exception {
+        //Setup
+        Assignature assignature = new Assignature();
+        assignature.setId(1L);
+        assignature.setName("software dev");
+        assignature.setCode("abc123");      
+        assignature.setCredits("4");
+        assignature.setStatus("active");
+        
+        // Mock the service save method
+        Mockito.when(service.save(Mockito.any(Assignature.class)))
+        .thenReturn(assignature);
+
+        //Execute
+        mockMvc.perform(
+            post("/assignature/save").contentType(MediaType.APPLICATION_JSON)
+                    .content(new ObjectMapper().writeValueAsString(assignature)))
+        //Assertions
+            .andExpect(status().isCreated())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("software dev"));
+    }
+
+    @Test
+    void testUpdate() throws Exception {
+       // Setup
+        Assignature assignature = new Assignature();
+        assignature.setId(1L);
+        assignature.setName("software dev");
+        assignature.setCode("abc123");
+        assignature.setCredits("4");
+        assignature.setStatus("active");
+
+        Assignature updatedAssignature = new Assignature();
+        updatedAssignature.setId(1L);
+        updatedAssignature.setName("software dev");
+        updatedAssignature.setCode("abc456");
+        updatedAssignature.setCredits("2");
+        updatedAssignature.setStatus("inactive");
+
+        // Mock the needed service methods
+        Mockito.when(service.findById(1L)).thenReturn(Optional.of(updatedAssignature));
+        Mockito.when(service.save(updatedAssignature)).thenReturn(updatedAssignature);
+
+        // Execute
+        mockMvc.perform(
+                put("/assignature/update")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(updatedAssignature))).andDo(print())                           
+        //Assert
+                .andExpect(MockMvcResultMatchers.status().isAccepted());
+                //.andExpect(MockMvcResultMatchers.jsonPath("$.name").value("software dev"))
+                //.andExpect(MockMvcResultMatchers.jsonPath("$.credits").value("2"))
+                //.andExpect(MockMvcResultMatchers.jsonPath("$.status").value("inactive"));
+    
+    }
+}
---
+++ grading_api/src/test/java/com/vanier/grading_api/controller/UserControllerTest.java
@@ -0,0 +1,174 @@
+package com.vanier.grading_api.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+//static imports required for the payload
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.vanier.grading_api.entity.Professor;
+import com.vanier.grading_api.entity.Student;
+import com.vanier.grading_api.service.ProfessorService;
+import com.vanier.grading_api.service.StudentService;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserControllerTest {
+
+    @MockBean
+    private ProfessorService professorService;
+
+    @MockBean
+    private StudentService studentService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testDeleteProfessor() throws Exception {
+        // Setup
+        Professor professor = new Professor();
+        professor.setId(1L);
+        professor.setFirstName("John");
+        professor.setLastName("Doe");
+        professor.setEmail("john.doe@example.com");
+
+        // Mock the service findById and delete method
+        Mockito.when(professorService.findById(Mockito.anyLong()))
+                .thenReturn(Optional.of(professor));
+        Mockito.doNothing().when(professorService).delete(Mockito.anyLong());
+
+        // Execute
+        mockMvc.perform(
+                delete("/professor/delete?id=1")).andDo(print())
+                // Assertions
+                .andExpect(status().isAccepted())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.firstName").value("John"));
+  
+
+    }
+
+    @Test
+    void testDeleteStudentNotModified() throws Exception {
+        // Setup
+        Student student = new Student();
+        student.setId(1L);
+        student.setFirstName("Jane");
+        student.setLastName("Doe");
+        student.setEmail("jane.doe@example.com");
+
+        // Mock the service findById and delete methods for not modified scenario
+        Mockito.when(studentService.findById(Mockito.anyLong()))
+                .thenReturn(Optional.empty());
+        Mockito.doNothing().when(studentService).delete(Mockito.anyLong());
+
+        // Execute
+        mockMvc.perform(
+                delete("/student/delete?id=1")).andDo(print())
+        // Assertions
+                .andExpect(status().isNotModified());
+    }
+
+    @Test
+    void testFindByIdProfessor() throws Exception {
+        // Setup
+        Professor professor = new Professor();
+        professor.setId(1L);
+        professor.setFirstName("John");
+        professor.setLastName("Doe");
+        professor.setEmail("john.doe@example.com");
+
+        // Mock the service findById method
+        Mockito.when(professorService.findById(Mockito.anyLong()))
+                .thenReturn(Optional.of(professor));
+
+        // Execute
+        mockMvc.perform(
+                get("/professor/findById?id=1")).andDo(print())
+        // Assert
+                .andExpect(status().isAccepted());
+
+    }
+
+    @Test
+    void testFindByIdStudentNotFound() throws Exception {
+        // Mock the service findById method for not found scenario
+        Mockito.when(studentService.findById(Mockito.anyLong()))
+                .thenReturn(Optional.empty());
+
+        // Execute
+        mockMvc.perform(
+                get("/student/findById?id=1")).andDo(print())
+        // Assert
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void testSaveProfessor() throws JsonProcessingException, Exception {
+        // Setup
+        Professor professor = new Professor();
+        professor.setId(1L);
+        professor.setFirstName("John");
+        professor.setLastName("Doe");
+        professor.setEmail("john.doe@example.com");
+
+        // Mock the service save method
+        Mockito.when(professorService.save(Mockito.any(Professor.class)))
+                .thenReturn(professor);
+
+        // Execute
+        mockMvc.perform(
+                post("/professor/save").contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(professor)))
+        // Assertions
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.firstName").value("John"));
+
+    }
+
+    @Test
+    void testUpdateProfessor() throws JsonProcessingException, Exception {
+        // Setup
+        Professor professor = new Professor();
+        professor.setId(1L);
+        professor.setFirstName("John");
+        professor.setLastName("Doe");
+        professor.setEmail("john.doe@example.com");
+
+        Professor updatedProfessor = new Professor();
+        updatedProfessor.setId(1L);
+        updatedProfessor.setFirstName("Updated John");
+        updatedProfessor.setLastName("Updated Doe");
+        updatedProfessor.setEmail("updated.john.doe@example.com");
+
+        // Mock the needed service methods
+        Mockito.when(professorService.findById(1L)).thenReturn(Optional.of(professor));
+        Mockito.when(professorService.save(updatedProfessor)).thenReturn(updatedProfessor);
+
+        // Execute
+        mockMvc.perform(
+                put("/professor/update")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(updatedProfessor)))
+        // Assert
+                .andExpect(MockMvcResultMatchers.status().isAccepted());
+                //.andExpect(MockMvcResultMatchers.jsonPath("$.firstName").value("Updated John"));
+    }
+
+}
